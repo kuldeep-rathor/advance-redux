ed30506a1377c058d1e47b53f33e354d37583a47
Kuldeep rathor <127830786+kuldeep-rathor@users.noreply.github.com>
2023-09-29 18:32:49 +0530

    cart-fixing

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,34 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
 
+const DUMMY_PRODUCTS = [
+  {
+    id: "p1",
+    price: 6,
+    title: "My First Ever Book",
+    description: "The First Ever Book I wrote ",
+  },
+  {
+    id: "p2",
+    price: 5,
+    title: "My Second Ever Book",
+    description: "The Second Ever Book I wrote ",
+  },
+];
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCTS.map((product) => (
+          <ProductItem
+            key={product.id}
+            id ={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
       </ul>
     </section>
   );
